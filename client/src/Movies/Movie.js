4eaccc213efdd10685044d9f91cd43ff320600c4
Jennie Van <missjenvan@gmail.com>
2019-09-17 14:31:51 -0700

    added save movie functionality and moviecard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState(null);
+
   useEffect(() => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
@@ -21,10 +23,10 @@ const Movie = (props) => {
   }, [props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -34,23 +36,13 @@ const Movie = (props) => {
 
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-        
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))} 
-      </div>
-      <div className="save-button">Save</div>
+      <MovieCard 
+        title={title} 
+        director={director} 
+        metascore={metascore} 
+        stars={stars} 
+      />
+      <div onClick={ saveMovie } className="save-button">Save</div>
     </div>
   );
 }
